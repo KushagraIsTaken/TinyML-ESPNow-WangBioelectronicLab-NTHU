1989733e0862baa4d458dd830c130d41ac259c0f
Kushagra Agrawal <kush4409@gmail.com>
2025-08-12 11:48:46 +0800

    Add Release

--- install_libs.bat
+++ install_libs.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmK4EolPxpaSwiDPgs=
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHGQ=
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQIROh9BRQqNfF+zCKwxxKjP4OWLqUQJNA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE31cjIBpTXgXCHWyoB6Id5Ig=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBBBXwzWAES0A5EO4f7+086IoVgQUewra7P806CmNeIv3kzqbLsBm19pqOUDG1ZvfxysTAY7rGJHtXCXOMmVsCPgSAWn0mMFJ2x6lHfRgCc+cp1tgsZj
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal
 
